3e7c741e359cafe877ca10b263997e623f417579
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2023-03-12 14:57:15 -0500

    Webhook

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const Whatsapp = require('./function/whastapp')
 
 exports.handler = async function (event, context, callback) {
     console.log("Event: ", event);
+    console.log("context: ", context);
     var whatsapp = new Whatsapp(process.env.TOKEN, process.env.WHATSAPPID);
-    var response = await whatsapp.sendMsj('573147139879', "bienvenida");
-    console.log(response);
+   // var response = await whatsapp.sendMsj('573147139879', "bienvenida");
+    this.response = {"hub_mode":"subscribe","hub_challenge":process.env.WHATSAPPID,"hub_verify_token":"metawhatsapp"};
+    //console.log(response);
     switch (event.httpMethod) {
         case 'GET':
             this.response = {"hub_mode":"subscribe","hub_challenge":process.env.WHATSAPPID,"hub_verify_token":"metawhatsapp"};
@@ -20,6 +22,6 @@ exports.handler = async function (event, context, callback) {
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
             "Content-Type": 'application/json'
         },
-        body: JSON.stringify(this.response.data)
+        body: JSON.stringify(this.response)
     };
 }
