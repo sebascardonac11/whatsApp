81e598a415b94f29e923e05026764375b30a8f05
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2023-03-12 20:18:55 -0500

    Webhook

--- index.js
+++ index.js
@@ -25,13 +25,6 @@ exports.handler = async function (event, context, callback) {
     }
     console.log("Response", this.response);
     return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
-            "Content-Type": 'application/json'
-        },
-        body: JSON.stringify(this.response)
+        statusCode: 200
     };
 }
