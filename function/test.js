1f36d50fdf29d646da941677876d757d89c394b2
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2023-03-12 20:37:58 -0500

    Webhook

--- function/test.js
+++ function/test.js
@@ -1,3 +1,53 @@
 
 const index = require('../index');
-index.handler({},{},{});
+index.handler({
+    resource: '/cda-Whatsapps/message',
+    path: '/cda-Whatsapps/message',
+    httpMethod: 'GET',
+    headers: {
+      Accept: '*/*',
+      'Accept-Encoding': 'deflate, gzip',
+      'CloudFront-Forwarded-Proto': 'https',
+      'CloudFront-Is-Desktop-Viewer': 'true',
+      'CloudFront-Is-Mobile-Viewer': 'false',
+      'CloudFront-Is-SmartTV-Viewer': 'false',
+      'CloudFront-Is-Tablet-Viewer': 'false',
+      'CloudFront-Viewer-ASN': '32934',
+      'CloudFront-Viewer-Country': 'US',
+      Host: 'jxl8rulxli.execute-api.us-east-2.amazonaws.com',
+      'User-Agent': 'facebookplatform/1.0 (+http://developers.facebook.com)',
+      Via: '1.1 7c6ba9a3a1fe14d52ffd0738978a0e6c.cloudfront.net (CloudFront)',
+      'X-Amz-Cf-Id': 'pUT2V7_HyoaSent1AOPafqsILpcRqTnFQCRRtAQfKKNoIn6FC5fQvg==',
+      'X-Amzn-Trace-Id': 'Root=1-640e7db9-79e6fa01544d75a416be7bbe',
+      'X-Forwarded-For': '69.63.184.116, 130.176.71.72',
+      'X-Forwarded-Port': '443',
+      'X-Forwarded-Proto': 'https'
+    },
+    multiValueHeaders: {
+      Accept: [ '*/*' ],
+      'Accept-Encoding': [ 'deflate, gzip' ],
+      'CloudFront-Forwarded-Proto': [ 'https' ],
+      'CloudFront-Is-Desktop-Viewer': [ 'true' ],
+      'CloudFront-Is-Mobile-Viewer': [ 'false' ],
+      'CloudFront-Is-SmartTV-Viewer': [ 'false' ],
+      'CloudFront-Is-Tablet-Viewer': [ 'false' ],
+      'CloudFront-Viewer-ASN': [ '32934' ],
+      'CloudFront-Viewer-Country': [ 'US' ],
+      Host: [ 'jxl8rulxli.execute-api.us-east-2.amazonaws.com' ],
+      'User-Agent': [ 'facebookplatform/1.0 (+http://developers.facebook.com)' ],
+      Via: [
+        '1.1 7c6ba9a3a1fe14d52ffd0738978a0e6c.cloudfront.net (CloudFront)'
+      ],
+      'X-Amz-Cf-Id': [ 'pUT2V7_HyoaSent1AOPafqsILpcRqTnFQCRRtAQfKKNoIn6FC5fQvg==' ],
+      'X-Amzn-Trace-Id': [ 'Root=1-640e7db9-79e6fa01544d75a416be7bbe' ],
+      'X-Forwarded-For': [ '69.63.184.116, 130.176.71.72' ],
+      'X-Forwarded-Port': [ '443' ],
+      'X-Forwarded-Proto': [ 'https' ]
+    },
+    queryStringParameters: {
+      'hub.challenge': '2098962047',
+      'hub.mode': 'subscribe',
+      'hub.verify_token': 'metawhatsapp'
+    }
+}
+    ,{},{});
